8dcfa1e4aa370d11df2f2e603d74b783ae565e5e
Vladimir Shumskikh <shumskikhv@gmail.com>
2024-09-12 11:47:16 +0500

    fourth commit

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,14 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 4; i ++)
             printSmth("loh", 2);
+        System.out.println(end);
     }
 
     public static void printSmth(String a, int b) {
         for (int i = 0; i < b; i++)
             System.out.println(a);
     }
+
+    public static String end = "Konec";
+
 }
