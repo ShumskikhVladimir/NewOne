247f47e5d97bd9a05aae8729fc4e81ea8d7cf9e0
Vladimir Shumskikh <shumskikhv@gmail.com>
2024-09-12 11:33:47 +0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 15; i += 2)
-            System.out.println(i);
+        for (int i = 0; i < 4; i ++)
+            printSmth("loh", 2);
+    }
+
+    public static void printSmth(String a1, int b) {
+        for (int i = 0; i <= b; i++)
+            System.out.println(a1);
     }
 }
